0d214d5012f1b356db5fdcfea395dc57fc337837
tanviriqbal <tanviriqbal4u@live.com>
2019-06-01 11:06:19 +0600

    Models in MVC

---
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Deparement { get; set; }
+    }
+
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int Id);
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Models/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        List<Employee> _employees;
+        public EmployeeRepository()
+        {
+            _employees = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="Mary",Deparement="HR",Email="mary@email.com"},
+                new Employee(){Id=1,Name="John",Deparement="IT",Email="john@email.com"},
+                new Employee(){Id=1,Name="Sam",Deparement="IT",Email="sam@email.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employees.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
