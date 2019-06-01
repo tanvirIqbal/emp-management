ebec0f4161974c8feaf3aa01bd11216cc4ced961
tanviriqbal <tanviriqbal4u@live.com>
2019-06-01 12:00:05 +0600

    Add Dependency Injection for EmployeeRepository

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,14 @@ namespace EmployeeManagement.Controllers
 {
     public class HomeController
     {
+        IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
         public string Index()
         {
-            return "Hello from MVC.";
+            return _employeeRepository.GetEmployee(1).Name;
         }
     }
 }
