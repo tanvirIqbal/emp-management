9a64fd25add8afaaa49350b9e2e15dd9fd92dd9d
tanviriqbal <tanviriqbal4u@live.com>
2019-08-14 15:09:46 +0600

    Upload Image

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +13,12 @@ namespace EmployeeManagement.Controllers
     //[Route("Home")]
     public class HomeController : Controller
     {
-        IEmployeeRepository _employeeRepository;
-        public HomeController(IEmployeeRepository employeeRepository)
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
+        public HomeController(IEmployeeRepository employeeRepository, IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
         //[Route("/")]
         //[Route("")]// Makes Index(), the default action
@@ -28,14 +32,43 @@ namespace EmployeeManagement.Controllers
         //[Route("Create")]
         public ViewResult Create()
         {
-            return View(new Employee());
+            return View(new EmployeeCreateViewModel());
         }
 
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
+                string uniqueFileName = null;
+
+                // If the Photo property on the incoming model object is not null, then the user
+                // has selected an image to upload. 
+                if (model.Photo != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot
+                    // To get the path of the wwwroot folder we are using the inject 
+                    // HostingEnvironment service provided by ASP.NET Core
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    // To make sure the file name is unique we are appending a new
+                    // GUID value and and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    // Use CopyTo() method provided by IFormFile interface to
+                    // copy the file to wwwroot/images folder
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    // Store the file name in PhotoPath property of the employee object
+                    // which gets saved to the Employees database table
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.Add(newEmployee);
                 return RedirectToAction("details", new { id = newEmployee.Id });
             }
 
---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Dept? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
